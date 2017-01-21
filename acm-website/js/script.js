815185b9fbb04719fc3060b05be1cb6c0161a348
ksaikiranr <ksaikiranr@gmail.com>
2017-01-22 00:16:57 +0530

    temp vesr

---
+++ acm-website/js/script.js
@@ -0,0 +1,28 @@
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 850);
+});
+
+
+
+$(document).ready(function(){
+  $('.slider1').bxSlider({
+    slideWidth: 500,
+    minSlides: 1,
+    maxSlides: 1,
+    slideMargin: 10
+  });
+});
