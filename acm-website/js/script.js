52d0cb86a1a58563ddd5f19f05ef13e0d6e6083f
ksaikiranr <ksaikiranr@gmail.com>
2017-02-13 22:44:50 +0530

    Initial version fixes

--- acm-website/js/script.js
+++ acm-website/js/script.js
@@ -49,4 +49,19 @@ $(document).ready(function(){
   });
     
 });
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
 
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
+
+
+document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+  });
