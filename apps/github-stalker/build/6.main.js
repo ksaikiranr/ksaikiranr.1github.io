a4f2a34c8fc10025491f4e85f7c7780876974137
sk <sk@localhost.localdomain>
2017-09-04 22:41:32 +0530

    added github stalker

---
+++ apps/github-stalker/build/6.main.js
@@ -0,0 +1,45 @@
+webpackJsonp([6],{
+
+/***/ 272:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(12);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__org_details__ = __webpack_require__(100);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "OrgDetailsPageModule", function() { return OrgDetailsPageModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var OrgDetailsPageModule = (function () {
+    function OrgDetailsPageModule() {
+    }
+    return OrgDetailsPageModule;
+}());
+OrgDetailsPageModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__org_details__["a" /* OrgDetailsPage */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__org_details__["a" /* OrgDetailsPage */]),
+        ],
+        exports: [
+            __WEBPACK_IMPORTED_MODULE_2__org_details__["a" /* OrgDetailsPage */]
+        ]
+    })
+], OrgDetailsPageModule);
+
+//# sourceMappingURL=org-details.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=6.main.js.map
